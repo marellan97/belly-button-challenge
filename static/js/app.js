12350fda17ca3c01bbca7cf7b88a9737dd34f940
marellan97 <125534160+marellan97@users.noreply.github.com>
2023-06-26 21:05:01 -0700

    Final Changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,29 @@
+function init() {
+  // Get the reference to the dropdown menu
+  let selector = d3.select("#selDataset");
+
+  let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+  // Use the list of sample names to populate the select options
+  d3.json(url).then((data) => {
+
+  // Do this by pulling the array associated with `names` 
+    let idNames = data.names;
+
+    // Loop through the names and append to the dropdown menu
+    for (let i = 0; i < idNames.length; i++){
+      selector.append("option").text(idNames[i]).property("value", idNames[i]);
+    };
+
+    // Use the first sample from the list to build the initial plots
+    let firstSample = idNames[0]
+    
+    buildBarChart(firstSample);
+    buildBubbleChart(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
 function buildMetadata(sample) {
     // Access the website and use d3 to operate on the data
     let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
@@ -20,90 +46,93 @@ function buildMetadata(sample) {
         for (key in result){
           panel.append("h6").text(`${key.toUpperCase()}: ${result[key]}`);
         };
-  
-      // If you want to do the bonus, you can make the gauge chart here
-  
-  
     });
   }
   
-  
-  function buildCharts(sample) {
+  function buildBarChart(sample) {
     // Access the website and use .then to operate on the data
     let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
       
-      d3.json(url).then((data) => {
+    d3.json(url).then((data) => {
   
-      // Filter the data for the object with the desired sample number (the id)
-        let sampleData = data.samples;
-        let res = sampleData.filter(sample_res => sample_res.id == sample);
-        let data_res = res[0]
+    // Filter the data for the object with the desired sample number (the id)
+    // Pull the desired information (ids, labels, values) from your filtered data
+    // Slice the data for your bar chart and order it (you can just use reverse)
+    // Build a Horizontal Bar Chart
+        let sampleInfo = data.samples;
+        let value = sampleInfo.filter(result => result.id == sample);
+        let valueData = value[0];
+    
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
 
-      // Pull the desired information (ids, labels, values) from your filtered data
-        let otu_ids = data_res.otu_ids;
-        let otu_labels = data_res.otu_labels;
-        let sample_values = data_res.sample_values;
-  
-      // Build a Bubble Chart
+        console.log(otu_ids, otu_labels, sample_values);
+        
+        let yticks = otu_ids.slice(0,10).map(id => `OTU ${id}`).reverse();
+        let xticks = sample_values.slice(0,10).reverse();
+        let labels = otu_labels.slice(0,10).reverse();
+           
+        let bar_chart = {
+            y: yticks,
+            x: xticks,
+            text:labels,
+            type:"bar",
+            orientation:"h",
+        };
+        
+        let layout = {
+            title: "Top 10"
+            
+        };
+
+        Plotly.newPlot("bar", [bar_chart], layout)
+      });
+  };
+   
+  function buildBubbleChart(sample) {
+      // Access the website and use .then to operate on the data
+      let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+        
+      d3.json(url).then((data) => {
+          
+        let sampleInfo = data.samples;
+        let value = sampleInfo.filter(result => result.id == sample);
+        let valueData = value[0];
+
+        let otu_ids = valueData.otu_ids;
+        let otu_labels = valueData.otu_labels;
+        let sample_values = valueData.sample_values;
+
+        console.log(otu_ids,otu_labels,sample_values);
+
         let bubble_chart = {
-          y: sample_values,
           x: otu_ids,
+          y: sample_values,
           text: otu_labels,
           mode: "markers",
           marker: {
-            size: sample_values,
-            color: otu_ids,
-            colorscale: "#FF4F00"
+              size: sample_values,
+              color: otu_ids,
           }
-        }
-
-        let layout={
-          margin: { t: 55, r: 35, l: 55, b: 65 },
-          title:"Bacteria Culture per sample",
-          hovermode:"closest",
-          xaxis:{title:"OTU ID"},
-          yaxis:{title:"Sample Value"},
-       };
-      var config = {responsive: true}
-      Plotly.newPlot("bubble",[bubble_chart],layout,config);
-    })
-  };
-   
-      // Slice the data for your bar chart and order it (you can just use reverse)
-      
-  
-      // Build a Horizontal Bar Chart
-     
-  function init() {
-    // Get the reference to the dropdown menu
-    let selector = d3.select("#selDataset");
-  
-    let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-  
-    // Use the list of sample names to populate the select options
-    d3.json(url).then((data) => {
-
-    // Do this by pulling the array associated with `names` 
-      let idNames = data.names;
-
-      // Loop through the names and append to the dropdown menu
-      for (let i = 0; i < idNames.length; i++){
-        selector.append("option").text(idNames[i]).property("value", idNames[i]);
       };
 
-      // Use the first sample from the list to build the initial plots
-      let firstSample = idNames[0]
-      
-      buildCharts(firstSample);
-      buildMetadata(firstSample);
+        let layout = {
+          title: "Bacteria Per Sample",
+          xaxis: {title: "OTU ID"},
+          
+        };
+
+        Plotly.newPlot("bubble", [bubble_chart], layout)
     });
-  }
-  
+   };
+
   function optionChanged(newSample) {
     // Change your data and update your plots/metadata when newSample is selected from the dropdown
-    buildCharts(newSample);
+    buildBarChart(newSample);
+    buildBubbleChart(newSample);
     buildMetadata(newSample);
-  }
+  };
   
   // Initialize the dashboard
   init();
